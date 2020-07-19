d7dc43cf26672d3006d452bfccd030996e79427e
Swapnil Kelkar <skelkar@purdue.edu>
2020-07-19 14:36:17 -0400

    updated shop data with new data, updated cart utils to group items in cart.reducer

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import './custom-button.styles.scss';
+import "./custom-button.styles.scss";
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
-  <button className={`${isGoogleSignIn ? 'google-sign-in': ''} custom-button`} {...otherProps} >
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverter,
+  ...otherProps
+}) => (
+  <button
+    className={`${inverter ? "inverter" : ""} ${
+      isGoogleSignIn ? "google-sign-in" : ""
+    } custom-button`}
+    {...otherProps}
+  >
     {children}
   </button>
-)
+);
 
 export default CustomButton;
