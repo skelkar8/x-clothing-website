977aac3910796dca17b8ecc3676c3893adf383b3
Swapnil Kelkar <skelkar@purdue.edu>
2020-07-22 21:36:43 -0400

    updated application with reselect, replaced cart selectors with memoized selectors

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,42 +1,42 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import './header.styles.scss';
-import {ReactComponent as Logo } from '../../assets/logo.svg';
-import {auth} from '../../firebase/firebase.utils';
-import {connect} from 'react-redux';
-import CartIcon from '../cart-icon/cart-icon.component';
-import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./header.styles.scss";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+import { auth } from "../../firebase/firebase.utils";
+import { connect } from "react-redux";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({currentUser, hidden}) => (
-  <div className='header'>
-    <Link className='logo-container' to='/'>
-      <Logo className='logo'/>
+const Header = ({ currentUser, hidden }) => (
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
     </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>
+    <div className="options">
+      <Link className="option" to="/shop">
         SHOP
       </Link>
-      <Link className='option' to='/shop'>
+      <Link className="option" to="/shop">
         CONTACT
       </Link>
-      {
-        currentUser?
-        <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-        :
-        <Link className='option' to='/signin'>SIGN IN</Link>
-      }
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
+        </Link>
+      )}
       <CartIcon />
     </div>
-    {
-      hidden? null: <CartDropdown />
-    }
+    {hidden ? null : <CartDropdown />}
   </div>
-)
+);
 
-const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
   currentUser,
   hidden
-})
-
+});
 
 export default connect(mapStateToProps)(Header);
